6f6538cdc4b0adac56f3fb0455f90ee4bd8fa1d5
Emir Moretti <emir.moretti@hotmail.com.ar>
2022-10-24 15:18:16 -0300

    contact component hook useState

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 import Greeting from './components/pure/Greeting';
 import GreetingF from './components/pure/GreetingF';
 import TaskListComponent from './components/container/Task_list';
+import { Contact } from './models/contact.class';
+import ContactComponent from './components/pure/ContactComponent';
 
 function App() {
+  
+  const newContact = new Contact('emir', 'moretti', 'emir@moretti.com', false);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +18,7 @@ function App() {
         {/*<Greeting name='Emir'></Greeting>*/}
         {/*<GreetingF name={'emir'}></GreetingF>*/}
         <TaskListComponent />
+        <ContactComponent contact={newContact}/>
       </header>
     </div>
   );
--- src/components/pure/ContactComponent.jsx
+++ src/components/pure/ContactComponent.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Contact } from '../../models/contact.class'
 
 const ContactComponent = ({contact}) => {
+
+  const [conected, setConected] = useState(contact.isConected);
+
+
+  const handleConected = () => {
+    setConected(!conected);
+  }
+  
   return (
     <div>
         <h1>nombre: {contact.name} apellido: {contact.lastName}</h1>
         <p>{contact.email}</p>
         <p>{contact.isConected}</p>
-
+        <p>Estado {conected ? 'Contacto En Línea' : 'Contacto No Disponible'}</p>
         <div>
             <span>Cambiar estado</span>
+            <button onClick={handleConected}>Click</button>
         </div>
     </div>
   )
